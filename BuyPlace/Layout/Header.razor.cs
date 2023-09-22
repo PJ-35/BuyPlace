bc26219db0848f044a3b8e04619b694d401e14f3
Pierre_Junior <sibafwafpj@gmail.com>
2023-09-22 01:05:33 -0400

    Componenet de header et footer finit

---
+++ BuyPlace/Layout/Header.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BuyPlace.Layout
+{
+    public partial class Header
+    {
+        bool collapseNavMenu = true;
+
+        string baseMenuClass = "navbar-collapse d-sm-inline-flex flex-sm-row-reverse";
+
+        string NavMenuCssClass => baseMenuClass + (collapseNavMenu ? " collapse" : "");
+
+        void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
