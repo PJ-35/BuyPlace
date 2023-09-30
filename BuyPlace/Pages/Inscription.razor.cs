77e4ddcc415e661c82494857c6ff3858d464a2fe
Wily Tatow <witly136@gmail.com>
2023-09-29 23:04:51 -0400

    fin Inscription

--- BuyPlace/Pages/Inscription.razor.cs
+++ BuyPlace/Pages/Inscription.razor.cs
@@ -6,8 +6,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-
-
+using System.ComponentModel.DataAnnotations;
 
 namespace BuyPlace.Pages
 {
@@ -17,12 +16,12 @@ namespace BuyPlace.Pages
 
         private User user = new User();
         private string cMdp = "";
-        public string LoginMesssage { get; set; }
+        public string LoginMesssage { get; set; } = "";
         UsersService userService=new UsersService();
         private string url;
 
         private long maxFileSize = 1024 * 1024 * 3; //3MB
-        private int maxAllowedFiles = 1;
+        private const int maxAllowedFiles = 1;
         private List<string> error = new List<string>();
         private IBrowserFile? file;
 
@@ -64,13 +63,10 @@ namespace BuyPlace.Pages
             if (file == null)
             {
                 return "";
-            }
-         
-
-            
+            }  
             try
             {
-                string newFileName = Path.ChangeExtension(Path.GetRandomFileName(), Path.GetExtension(file.Name));
+                string newFileName = Path.ChangeExtension(Path.GetRandomFileName(), ".jpg");
                 string path = Path.Combine(config.GetValue<string>("FileStorage")!, newFileName);
                 string relativePath = Path.Combine(newFileName);
                 //Directory.CreateDirectory(Path.Combine(config.GetValue<string>("FileStorage")!, $"{user.UserName}"));
@@ -91,7 +87,7 @@ namespace BuyPlace.Pages
             
         }
 
-        private void SignInUser()
+        private async Task SignInUser()
         {
             var userobj = user;
             string rgxMdp = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$";
@@ -101,59 +97,118 @@ namespace BuyPlace.Pages
             // NavManager.NavigateTo("/");
 
             var editContext = new EditContext(user);
-            var isValid = editContext.Validate();
+            var validationResults = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, new ValidationContext(user), validationResults, true);
+
 
             if (url == "inscription")
             {
-
                 if (isValid)
                 {
-                    if (cMdp == userobj.Mdp)
+                    if (string.IsNullOrWhiteSpace(user.UserName)|| string.IsNullOrWhiteSpace(user.Courriel) || string.IsNullOrWhiteSpace(user.Mdp))
                     {
-                        if (Regex.IsMatch(userobj.Mdp, rgxMdp))
-                        {
-
-                            user.IsAdmin = false;
-                            user.IsCompleted = false;
-                            //userService.Save(user);
-
-                           // NavManager.NavigateTo("/next");
-                            url = "next";
-
-                            InvokeAsync(StateHasChanged);
-
-
-                        }
-                        else
-                            LoginMesssage = "Votre mot de passe doit contenir au moins 8 caractères, une lettre majiscule, au moins une lettre minuscule,au moins un chiffre .";
+                        LoginMesssage = "Veuillez remplir tous les champs";
+                    }
+                   
+                }
+                else
+                {
+                    if (!(string.IsNullOrWhiteSpace(user.UserName) || string.IsNullOrWhiteSpace(user.Courriel) || string.IsNullOrWhiteSpace(user.Mdp)))
+                    {
+                        LoginMesssage = "";
+                        //InvokeAsync(StateHasChanged);
                     }
                     else
-                    {
-                        LoginMesssage = "Veuillez confirmer votre mot de passe.";
-                    }
-
+                        LoginMesssage = "Veuillez remplir tous les champs";
                 }
-                else 
+                if (LoginMesssage == "" && cMdp!=user.Mdp)
                 {
-                    // Les données ne sont pas valides, affichez un message d'erreur
-                    LoginMesssage = "Veuillez corriger les erreurs du formulaire.";
+                    LoginMesssage = "Veuillez mettre des mots de passe identique";
+                    user.Mdp = cMdp = "";
                 }
+                else if (isValid && cMdp == user.Mdp)
+                {
+                    LoginMesssage = "";
+                }
+
+                if (isValid && LoginMesssage == "")
+                {
+                    url = "next";
+
+                    //InvokeAsync(StateHasChanged);
+                }
+
+
+                //if (isValid)
+                //{
+                //    if (cMdp == userobj.Mdp)
+                //    {
+                //        //if (Regex.IsMatch(userobj.Mdp, rgxMdp))
+                //        //{
+
+                //        user.IsAdmin = false;
+
+                //        //userService.Save(user);
+
+                //        // NavManager.NavigateTo("/next");
+                //        url = "next";
+
+                //        InvokeAsync(StateHasChanged);
+
+
+                //        //}
+                //        //else
+                //        //    LoginMesssage = "Votre mot de passe doit contenir au moins 8 caractères, une lettre majiscule, au moins une lettre minuscule,au moins un chiffre .";
+                //    }
+                //    else
+                //    {
+                //        LoginMesssage = "Veuillez confirmer votre mot de passe.";
+                //    }
+
+                //}
+                //else 
+                //{
+                //    // Les données ne sont pas valides, affichez un message d'erreur
+                //    LoginMesssage = "Veuillez corriger les erreurs du formulaire.";
+                //}
             }
             else if(url == "next" )
             {
 
-              
+                LoginMesssage = "";
                 if (isValid)
                 {
-                    string relativePath = CaptureFiles().Result;
-                    userService.Save(user);
-                    NavManager.NavigateTo("/");
+                    if (string.IsNullOrWhiteSpace(user.Nom) || string.IsNullOrWhiteSpace(user.Prenom))
+                    {
+                        LoginMesssage = "Veuillez remplir tous les champs";
+                    }
+
                 }
                 else
                 {
-                    // Les données ne sont pas valides, affichez un message d'erreur
-                    LoginMesssage = "Veuillez corriger les erreurs du formulaire.";
+                    if (!(string.IsNullOrWhiteSpace(user.Nom) || string.IsNullOrWhiteSpace(user.Prenom)))
+                    {
+                        LoginMesssage = "";
+                    }
+                    else
+                        LoginMesssage = "Veuillez remplir tous les champs";
                 }
+
+                if (LoginMesssage=="" && error.Count < 1)
+                {
+                    string relativePath = await CaptureFiles();
+                    userService.Save(user);
+                    NavManager.NavigateTo("/");
+                }
+
+                //string relativePath = await CaptureFiles();
+                //userService.Save(user);
+                //NavManager.NavigateTo("/");
+                //else
+                //{
+                //    // Les données ne sont pas valides, affichez un message d'erreur
+                //    LoginMesssage = "Veuillez corriger les erreurs du formulaire.";
+                //}
             }
 
 
