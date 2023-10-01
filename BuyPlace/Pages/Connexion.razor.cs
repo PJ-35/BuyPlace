f3b7f9c308d5abdf6bebe959f36cbf3a4f3d8a46
Wily Tatow <witly136@gmail.com>
2023-09-30 23:27:45 -0400

    formulaire de connexion

---
+++ BuyPlace/Pages/Connexion.razor.cs
@@ -0,0 +1,14 @@
+﻿using BuyPlace.Data;
+using BuyPlace.Service;
+
+namespace BuyPlace.Pages
+{
+    public partial class Connexion
+    {
+
+        private User user = new User();
+      
+        public string LoginMesssage { get; set; } = "";
+        private UsersService userService = new UsersService();
+    }
+}
