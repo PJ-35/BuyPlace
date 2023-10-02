5404f7f7ae5b420f78352f1104eb98fda920d65f
Wily Tatow <witly136@gmail.com>
2023-10-02 00:27:07 -0400

    formulaire de connexion

--- BuyPlace/Pages/Connexion.razor.cs
+++ BuyPlace/Pages/Connexion.razor.cs
@@ -1,5 +1,9 @@
 ﻿using BuyPlace.Data;
 using BuyPlace.Service;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using System;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BuyPlace.Pages
 {
@@ -10,5 +14,22 @@ namespace BuyPlace.Pages
       
         public string LoginMesssage { get; set; } = "";
         private UsersService userService = new UsersService();
+
+
+        [Inject]
+        private NavigationManager NavManager { get; set; }
+
+        [Inject]
+        ILocalStorageService localStorage { get; set; }
+
+        [Inject]
+        AuthenticationStateProvider AuthStateProvider { get; set; } 
+        private async void HandleLogin()
+        {
+            await localStorage.SetItemAsync<string>("username", user.UserName);
+            await AuthStateProvider.GetAuthenticationStateAsync();
+            NavManager.NavigateTo("");
+            // LocalStorage.SetI
+        }
     }
 }
